2af3a4e74294409a3133ac5fa156dbabed43f3e8
muhammad-khaled-tech <muhammad.khaled.tech@gmail.com>
2026-06-06 20:41:00 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/aws-ccp-practice-exam/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
